3132a0a17f2cbe3da5dbf8a92f1ed7193646d30c
Dvirco <98239074+dvirco123@users.noreply.github.com>
2023-12-17 18:13:12 -0500

    Update addCommas.js

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,34 @@
-function addCommas() {}
+function addCommas(num) {
+  let sign = "";
+  
+  if (num < 0) sign = "-";
+  num = Math.abs(num);
+  
+  let [whole, decimal] = num.toString().split(".");
+
+  if (decimal) {
+    return `${sign}${commaInsert(whole)}.${decimal}`;
+  }
+
+  return `${sign}${commaInsert(whole)}`;
+}
+
+function commaInsert(str) {
+ 
+  let commaIndex = 0;
+  let commaStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    commaStr = str[i] + commaStr;
+    commaIndex++;
+     
+    if (commaIndex % 3 === 0 && i !== 0) {
+      commaStr = "," + commaStr;
+    }
+  }
+
+  return commaStr;
+}
 
 module.exports = addCommas;
+
